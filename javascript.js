8420e1f4cb991142c044e3dc65b5fc403f622fa2
Jeanvally G.Beato <jeanvallybeato@gmail.com>
2023-08-17 20:57:45 -0700

    Game function added.

--- javascript.js
+++ javascript.js
@@ -21,22 +21,22 @@ function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
 
     if(playerSelection == "scissors" && computerSelection == "rock"){
-        return "You Lose! Rock beats Scissors";
+        return "computer";
     }
     if(playerSelection == "paper" && computerSelection == "scissors"){
-        return "You Lose! Scissors beats Paper";
+        return "computer";
     }
     if(playerSelection == "rock" && computerSelection == "paper"){
-        return "You Lose! Paper beats Rock";
+        return "computer";
     }
     if(computerSelection == "scissors" && playerSelection == "rock"){
-        return "You Win! Rock beats Scissors";
+        return "player";
     }
     if(computerSelection == "paper" && playerSelection == "scissors"){
-        return "You Win! Scissors beats Paper";
+        return "player";
     }
     if(computerSelection == "rock" && playerSelection == "paper"){
-        return "You Win! Paper beats Rock";
+        return "player";
     }
     else {
         return "No Winner!  Play again!";
@@ -44,5 +44,98 @@ function playRound(playerSelection, computerSelection){
 
 }
 
-console.log(getComputerChoice());
-console.log(playRound("rock",getComputerChoice()));
+function game() {
+
+    let playerScore = 0;
+    let computerScore = 0;
+    let roundResults;
+
+    //ROUND 1
+    let playerSelection = prompt("Choose by typing rock, paper or scissors");
+    roundResults = playRound(playerSelection,getComputerChoice());
+    if(roundResults == "player"){
+        playerScore++;
+        console.log("You win this round!");
+    }
+    else if(roundResults = "computer"){
+        computerScore++;
+        console.log("Computer wins this round!");
+    }
+    else {
+        console.log("No winner this round...");
+    }
+
+    //ROUND 2
+    playerSelection = prompt("Choose by typing rock, paper or scissors");
+    roundResults = playRound(playerSelection,getComputerChoice());
+    if(roundResults == "player"){
+        playerScore++;
+        console.log("You win this round!");
+    }
+    else if(roundResults = "computer"){
+        computerScore++;
+        console.log("Computer wins this round!");
+    }
+    else {
+        console.log("No winner this round...");
+    }
+
+    //ROUND 3
+    playerSelection = prompt("Choose by typing rock, paper or scissors");
+    roundResults = playRound(playerSelection,getComputerChoice());
+    if(roundResults == "player"){
+        playerScore++;
+        console.log("You win this round!");
+    }
+    else if(roundResults = "computer"){
+        computerScore++;
+        console.log("Computer wins this round!");
+    }
+    else {
+        console.log("No winner this round...");
+    }
+
+    //ROUND 4
+    playerSelection = prompt("Choose by typing rock, paper or scissors");
+    roundResults = playRound(playerSelection,getComputerChoice());
+    if(roundResults == "player"){
+        playerScore++;
+        console.log("You win this round!");
+    }
+    else if(roundResults = "computer"){
+        computerScore++;
+        console.log("Computer wins this round!");
+    }
+    else {
+        console.log("No winner this round...");
+    }
+
+    //ROUND 5
+    playerSelection = prompt("Choose by typing rock, paper or scissors");
+    roundResults = playRound(playerSelection,getComputerChoice());
+    if(roundResults == "player"){
+        playerScore++;
+        console.log("You win this round!");
+    }
+    else if(roundResults = "computer"){
+        computerScore++;
+        console.log("Computer wins this round!");
+    }
+    else {
+        console.log("No winner this round...");
+    }
+
+    //DECLARE WINNER OF GAME
+    if(computerScore > playerScore){
+        console.log("Computer WINS!");
+    }
+    else if(playerScore > computerScore){
+        console.log("You WIN!");
+    }
+    else {
+        console.log("No winner....THE END");
+    }
+    
+}
+
+game();
