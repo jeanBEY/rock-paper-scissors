d9400c4e36a58b05ced1cb9a13109f0d5e8d4e4e
Jeanvally G.Beato <jeanvallybeato@gmail.com>
2023-08-17 20:14:18 -0700

    Round function added.

--- javascript.js
+++ javascript.js
@@ -16,4 +16,33 @@ function getComputerChoice() {
     return compChoice;
 }
 
+function playRound(playerSelection, computerSelection){
+
+    playerSelection = playerSelection.toLowerCase();
+
+    if(playerSelection == "scissors" && computerSelection == "rock"){
+        return "You Lose! Rock beats Scissors";
+    }
+    if(playerSelection == "paper" && computerSelection == "scissors"){
+        return "You Lose! Scissors beats Paper";
+    }
+    if(playerSelection == "rock" && computerSelection == "paper"){
+        return "You Lose! Paper beats Rock";
+    }
+    if(computerSelection == "scissors" && playerSelection == "rock"){
+        return "You Win! Rock beats Scissors";
+    }
+    if(computerSelection == "paper" && playerSelection == "scissors"){
+        return "You Win! Scissors beats Paper";
+    }
+    if(computerSelection == "rock" && playerSelection == "paper"){
+        return "You Win! Paper beats Rock";
+    }
+    else {
+        return "No Winner!  Play again!";
+    }
+
+}
+
 console.log(getComputerChoice());
+console.log(playRound("rock",getComputerChoice()));
